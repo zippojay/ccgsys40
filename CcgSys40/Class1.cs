feef4e96bcee73a4dc72c1e26e96937f81b5d27e
zippojay <zippojay@DESKTOP-NJUG2RG>
2018-11-24 08:10:46 +0800

    20181124

---
+++ CcgSys40/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CcgSys40
+{
+    public class Class1
+    {
+        public static string a ="a";
+    }
+}
